00d3661c9f378cdd3c4db27ca17858fd5dfa3f1e
Chocobe <nice_10bird@naver.com>
2019-09-29 18:06:43 +0900

    <master> : Created class SecondServlet

--- edu/src/com/edu/test/SecondServlet.java
+++ edu/src/com/edu/test/SecondServlet.java
@@ -1,18 +1,29 @@
 package com.edu.test;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class SecondServlet extends HttpServlet {
 	@Override
-	public void init(ServletConfig config) {
-		System.out.println("init() 실행!");
-	}
-	
-	@Override
-	public void service(ServletRequest arg0, ServletResponse arg1) {
-		System.out.println("service() 실행!");
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+					throws ServletException, IOException {
+		System.out.println("SecondsServlet !");
+		PrintWriter out = resp.getWriter();
+		out.print(
+				"<!DOCTYPE html>" +
+				"<html lang=\"ko\">" +
+				"<head>" +
+				"<title>Test</title>" +
+				"</head>" +
+				"<body>" +
+				"<h1>have a nice day !!</h1>" +
+				"</body>" +
+				"</html>");
+		out.close();
 	}
 }
