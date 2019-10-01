02f2053871d3007aba81989b9d02175d571a1cc4
Chocobe <nice_10bird@naver.com>
2019-10-01 12:29:34 +0900

    <master> : Fixed class ServletContextTest1Servlet

--- edu/src/com/edu/test/ServletContextTest1Servlet.java
+++ edu/src/com/edu/test/ServletContextTest1Servlet.java
@@ -3,7 +3,6 @@ package com.edu.test;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,19 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/context1")
 public class ServletContextTest1Servlet extends HttpServlet {
-	ServletContext sc;
-	
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		sc = config.getServletContext();
-	}
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 					throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
 		
+		ServletContext sc = this.getServletContext();
 		out.print("Context : " + sc);
 		
 		out.close();
